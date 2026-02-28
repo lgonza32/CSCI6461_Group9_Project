adc98935333df710879ea37ea3c070a67f18df86
Laurenz Luke Gonzales <laurenzpetilla@gmail.com>
2026-02-28 15:00:54 -0500

    added controller folder to handle actions

--- src/simulator/ui/GUI.java
+++ src/simulator/ui/GUI.java
@@ -6,7 +6,6 @@ import javax.swing.text.AbstractDocument;
 import javax.swing.text.AttributeSet;
 import javax.swing.text.BadLocationException;
 import javax.swing.text.DocumentFilter;
-
 import java.awt.*;
 
 /**
